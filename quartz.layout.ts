4f706accb07e511e1330c0451154f44c1f06d401
bear0 <walkingbear42@gmail.com>
2024-07-22 18:52:09 +0900

    Quartz sync: Jul 22, 2024, 6:52 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,6 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
+  afterBody: [],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
@@ -27,14 +28,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
-    /*Component.DesktopOnly(
-        Component.RecentNotes({
-            title: "Recents",
-            limit: 10,
-            showTags: false,
-            linkToMore: "contents/" as SimpleSlug,
-        }),
-    ),*/
   ],
   right: [
     Component.Graph({
@@ -59,14 +52,6 @@ export const defaultListPageLayout: PageLayout = {
     Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
-    /*Component.DesktopOnly(
-        Component.RecentNotes({
-            title: "Recents",
-            limit: 10,
-            showTags: false,
-            linkToMore: "contents/" as SimpleSlug,
-        }),
-    ),*/
   ],
   right: [],
 }
